d409aa44e3be02201f1820de6694fe464ea08337
PonMorin <pon.m22@gmail.com>
2025-09-25 11:46:13 +0700

    Remove state.py as part of project restructuring

--- src/state.py
+++
@@ -1,9 +0,0 @@
-from typing import TypedDict
-from pandas import DataFrame
-
-# Create State
-class GraphState(TypedDict):
-    raw_text: str
-    result_summarize: str
-    audio_uri: str
-    gcs_output_path: str
