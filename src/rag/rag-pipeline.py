0865cb5e86a87992fd69577a9624aae66d5bf6a0
Chonakan Chumtap <cshuhand@gmail.com>
2025-09-25 09:59:47 +0700

    Fix import paths for GCP client and RagFunc in rag-pipeline.py

--- src/rag/rag-pipeline.py
+++ src/rag/rag-pipeline.py
@@ -1,5 +1,5 @@
 import os
-from Config.gcp import load_credentials_base64
+from config.gcp import load_credentials_base64
 from dotenv import load_dotenv
 load_dotenv()
 from rag.function import load_document, process_document, init_vector_store
